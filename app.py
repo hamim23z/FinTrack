d58ae016c29bdf6291cc92b00cfbac8ac3bec81c
Hamim <hamimc232@gmail.com>
2025-10-24 19:45:04 -0400

    Made the Expenses DB and also fixed the form. Did some testing

--- app.py
+++ app.py
@@ -1,9 +1,42 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, url_for, make_response
+from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 app = Flask(__name__)
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(120), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(75), nullable=False)
+    date = db.Column(db.Date, nullable=False, default=date.today)
+
+
+
+with app.app_context():
+    db.create_all()
+
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+
+@app.route("/add", methods=['POST'])
+def add():
+    print("Form received:", dict(request.form) )
+    return make_response("Form received, check console")
+
+
 
 if (__name__) == "__main__":
     app.run(debug=True)
