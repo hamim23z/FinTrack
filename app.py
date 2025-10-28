d936984e7e79b3751f53fdcbeb7724e24d026128
Hamim <hamimc232@gmail.com>
2025-10-28 17:53:02 -0400

    All filters now work properly, including the date and categories. Set a default date on the screen as todays date

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def parse_date_or_none(s: str):
 def index():
     start_str = (request.args.get("start") or "").strip()
     end_str = (request.args.get("end") or "").strip()
+    selected_category = (request.args.get("category") or "").strip()
 
     start_date = parse_date_or_none(start_str)
     end_date = parse_date_or_none(end_str)
@@ -59,6 +60,8 @@ def index():
         q = q.filter(Expense.date >= start_date)
     if end_date:
         q = q.filter(Expense.date <= end_date)
+    if selected_category:
+        q = q.filter(Expense.category == selected_category)
 
 
     expenses = q.order_by(Expense.date.desc(), Expense.id.desc()).all()
@@ -70,7 +73,8 @@ def index():
         expenses=expenses,
         total=total,
         start_str = start_str,
-        end_str = end_str
+        end_str = end_str,
+        selected_category=selected_category
     )
 
 
